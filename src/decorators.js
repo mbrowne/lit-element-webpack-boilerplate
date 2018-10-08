b4c88dc7cc1d16aa0c106a7a2ff360f8922d935d
Matthew Browne <mbrowne83@gmail.com>
2018-10-08 00:24:31 -0400

    better implementation of property() decorator

--- src/decorators.js
+++ src/decorators.js
@@ -1,12 +1,16 @@
 // lit-element's @property decorator, upgraded for Babel 7 and current decorators proposal
 export function property(options) {
     return elementDescriptor => {
+        const name = elementDescriptor.key
+        // key generation code copied from https://github.com/Polymer/lit-element/blob/master/src/lib/updating-element.ts
+        const key = typeof name === 'symbol' ? Symbol() : `__${name}`
         return {
+            // We are creating an own property and using the original initializer, but changing the key,
+            // so foo becomes __foo. The getter and setter methods are created by createProperty().
             ...elementDescriptor,
-            // TEMP WORKAROUND - rename instance field
-            key: '__' + elementDescriptor.key,
-            finisher(klass) {
-                klass.createProperty(elementDescriptor.key, options)
+            key,
+            finisher(clazz) {
+                clazz.createProperty(name, options)
             }
         }
     }
